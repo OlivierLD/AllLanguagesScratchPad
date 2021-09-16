0bbe4daf1354dcf85d935f79ceb34a0706b1d97b
OlivierLD <olivier@lediouris.net>
2021-09-16 10:48:49 -0700

    Refactoring

---
+++ misc-tests/src/main/java/oliv/scratch/ScratchPad.java
@@ -0,0 +1,38 @@
+package oliv.scratch;
+
+import utils.DumpUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+
+public class ScratchPad {
+
+    public static void main(String... args) {
+        Map<String, String> map = Map.of("Key", "value", "Akeu", "Coucou");
+        map.keySet().forEach(k -> System.out.printf("%s -> %s %n", k, map.get(k)));
+
+        File nmeaFile = new File("./data.nmea");
+        ByteArrayOutputStream bos = null;
+        try {
+            FileInputStream fis = new FileInputStream(nmeaFile);
+            byte[] buffer = new byte[1_024];
+            bos = new ByteArrayOutputStream();
+            int len;
+            while ((len = fis.read(buffer)) != -1) {
+                bos.write(buffer, 0, len);
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+        if (bos != null) {
+            byte[] bytes = bos.toByteArray();
+            String[] dd = DumpUtil.dualDump(bytes);
+            for (String l : dd) {
+                System.out.println(l);
+            }
+        }
+    }
+}
