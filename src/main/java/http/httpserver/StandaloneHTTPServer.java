558ca11b9b9be166d4afcba33927bffb3b318321
OlivierLD <olivier@lediouris.net>
2021-11-11 13:36:28 -0800

    Imports.

--- src/main/java/http/httpserver/StandaloneHTTPServer.java
+++ src/main/java/http/httpserver/StandaloneHTTPServer.java
@@ -1,6 +1,11 @@
 package http.httpserver;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
