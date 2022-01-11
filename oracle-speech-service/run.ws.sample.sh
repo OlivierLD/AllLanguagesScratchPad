208349a2ad234f488c02d5b51f50c368869f14a6
OlivierLD <olivier@lediouris.net>
2022-01-11 13:55:22 +0100

    Added access to the SpeechService

--- oracle-speech-service/run.ws.sample.sh
+++ oracle-speech-service/run.ws.sample.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CP=./build/libs/SpeechJavaSDK_01-1.0-all.jar
+CP=./build/libs/oracle-speech-service-1.0-all.jar
 LOGGING_FLAGS="-Djava.util.logging.config.file=./logging.properties"
 # LOGGING_FLAGS="-Dlog4j.debug=true -Dlog4j.configuration=file:///log4j.properties"
 # LOGGING_FLAGS="-Dlog4j.debug=true" # Will use the log4j.properties located in the resources folder
