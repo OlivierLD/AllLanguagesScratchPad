cd0e4329f28dd3d054a67f4c3074c466978ffb05
OlivierLD <olivier@lediouris.net>
2022-04-08 08:42:20 +0200

    Added a sample

--- misc-tests/es6/nodejs/args.js
+++ misc-tests/es6/nodejs/args.js
@@ -6,6 +6,7 @@ console.log(`args: ${args}`);
 console.log(`h: ${h}`);
 
 args = [7, 3, 8]; // Reset to original
+// ... returns the content of the array, element by element.
 [h] = [...args].reverse(); // args is still 7, 3, 8
 console.log(`args: ${args}`);
 console.log(`h: ${h}`);
