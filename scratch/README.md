480ca386ec829839d6ca66ad9c03e58f103cc081
OlivierLD <olivier@lediouris.net>
2023-03-15 18:45:24 +0100

    Maven tests.

---
+++ scratch/README.md
@@ -0,0 +1,2 @@
+# Yo!
+This is a test, for the maven repo hosted in ROB (<https://raw.githubusercontent.com/OlivierLD/ROB/repository>)
---
+++ scratch/src/main/java/scratch/DewPoint.java
@@ -0,0 +1,16 @@
+package scratch;
+
+import utils.WeatherUtil;
+
+public class DewPoint {
+
+    public static void main(String... args) {
+        double airTemp = 20.0;
+        double relHum = 65.0;
+
+        double dewPointTemp = WeatherUtil.dewPointTemperature(relHum, airTemp);
+        double rounded = Math.round(dewPointTemp * 10) / 10.0;
+        System.out.printf("Dew Point Temp: %f\272C => Rounded %f\272C\n", dewPointTemp, rounded);
+    }
+
+}
